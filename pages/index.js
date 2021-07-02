f2660767db8566d8cab1c1f72fb05fe4f90a04fa
Christy Campbell <christytc10@gmail.com>
2021-07-02 08:29:45 +0100

    Show vods from followed streams json

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,47 @@
 import Head from 'next/head'
-import Image from 'next/image'
-
 import TwitchApi from "../lib/TwitchApi";
+import React, {useState, useEffect} from 'react';
+
+const millisInDay = 86400000;
 
 export async function getServerSideProps(context) {
-    console.log("Server Side");
     const myUser = 604277296 //my own user as an example
     const token = await TwitchApi.getAccessToken();
-    const user_follows = await TwitchApi.getVodsForUserFollows(myUser, token)
-    console.log(user_follows)
+    const user_follows = await TwitchApi.getUserFollows(myUser, token.access_token)
     return {
         props: {
             follows: user_follows,
+            token: token,
+            clientId: process.env.TWITCH_CLIENT_ID
         },
     }
 }
 
-export default function Home({follows}) {
+function VodPanel(props) {
+    // TODO - useState/useEffect hooks
+    // TODO - split into own file
+    const [vod, setVod] = useState(undefined);
+
+    useEffect(async () => {
+        if (!vod) {
+            const vods = await TwitchApi.getVodsForChannel(props.channel, props.token.access_token, props.clientId)
+            const latestVod = vods.data[0]
+            if (latestVod && Date.now() - Date.parse(latestVod.created_at) < millisInDay && latestVod.thumbnail_url) {
+                setVod(latestVod)
+            }
+        }
+    });
+
+    return (
+        <div>
+            <pre id="json">
+                {JSON.stringify(vod, undefined, 2)}
+            </pre>
+        </div>
+    );
+}
+
+export default function Home({follows, token, clientId}) {
     return (
         <div>
             <Head>
@@ -31,9 +56,8 @@ export default function Home({follows}) {
                 </h1>
                 <ul>
                     {follows.map((follow) => (
-                        <li>
-                            {follow.followedUserDisplayName} - <a href={follow.vod.url}>{follow.vod.title}</a>
-                            <Image src={follow.vod.thumbnail} alt='Some text' width='128px' height='128px'/>
+                        <li key={follow.to_login}>
+                            <VodPanel channel={follow.to_id} token={token} clientId={clientId}/>
                         </li>
                     ))}
                 </ul>
